70f8e32e0a6557fa1cff77d39b4b370a6db950c1
Darshan Karkar <dkarkar@gmail.com>
2013-12-13 11:31:03 -0500

    readme stuff?

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* 
 	By Darshan Karkar
-	12/8/2013
+	12/18/2013
 	CSIT 570
 	
 	Created using Jquery and a JSON feed of superheroes
@@ -10,26 +10,37 @@
 */
 
 $(document).ready(function() {
-	
-	/*
-	  ______   ______   ______   _____  
-	 |  ____| |  ____| |  ____| |  __ \ 
-	 | |__    | |__    | |__    | |  | |
-	 |  __|   |  __|   |  __|   | |  | |
-	 | |      | |____  | |____  | |__| |
-	 |_|      |______| |______| |_____/ 
-	
-	*/
-	
-	
-	//var arrayNames = [ "superhero", "publisher", "alter_ego", "first_appearance", "characters" ];
-	
- // 	$( "#attr" ).click(function() {
-	// 	var select = $( "#attr" ).val();
-	// 	console.log(arrayNames[2]);
-	// });
 
-	$('#search').typeahead({                                
+	//check for select field change, redo typeahead autocomplete if detected.
+ 	$( "#attr" ).change(function() {
+		var select = $( "#attr" ).val();
+		
+		//destroy previous autocomplete values
+		$('#search').typeahead('destroy');
+		
+		//readd relevent typeahead autocomplete values depending on selected value
+		$('#search').typeahead({                                
+			name: [select],
+			prefetch: 
+			{
+				url: 'js/superheroes.json',
+				filter: function(parsedResponse){
+					var dataset = [];
+					for(i = 0; i < parsedResponse.superheroes.length; i++) {
+						dataset.push({
+							select: parsedResponse.superheroes[i][select],
+							value: parsedResponse.superheroes[i][select],
+							tokens: [parsedResponse.superheroes[i][select]]
+						});
+					}
+					return dataset;
+				}
+			}
+		});		
+	});
+
+	//onload prefetch superhero names.
+ 	$('#search').typeahead({                                
 		name: "heroes3",
 		prefetch: 
 		{
@@ -47,78 +58,58 @@ $(document).ready(function() {
 			}
 		}
 	});
-
+	
+	//jquery get json feed
 	$.getJSON("js/superheroes.json", function(data) {
+		//loop for the main feed. 
 		$.each(data.superheroes, function(k, v) {
 			var list = data.superheroes[k];
 			$("#superheroes").append("<div class='hero' name='superhero' id="+list.superhero+">"+
-									 "<div class='details'>"+
-									 "<h1>"+list.superhero+"</h1>"+
-									 "<br>"+
-									 "<span id='publisher'>Publisher: </span>"+list.publisher+
-									 "<br>"+
-									 "<span id='alterEgo'>Alter ego: </span>"+list.alter_ego+
-									 "<br>"+
-									 "<span id='firstAppearance'>First Appearance: </span>"+list.first_appearance+
-									 "<br>"+
-									 "<span id='characters'>Portrayed by: </span>"+list.characters+
-									 "<br></div><div class='img'>"+
-									 "<img src='"+list.artURL+"'>"+
-									 "</div></div>");
+										"<div class='details'>"+
+											"<h1>"+list.superhero+"</h1>"+"<br>"+
+											"<span id='publisher'>Publisher: </span>"+list.publisher+"<br>"+
+											"<span id='alterEgo'>Alter ego: </span>"+list.alter_ego+"<br>"+
+											"<span id='firstAppearance'>First Appearance: </span>"+list.first_appearance+"<br>"+
+											"<span id='characters'>Portrayed by: </span>"+list.characters+"<br>"+
+										"</div>"+
+										"<div class='img'>"+
+											"<img src='"+list.artURL+"'>"+
+										"</div>"+
+									 "</div>");
 		});
-	});
-	
-	
-	/*
-	   _____   ______              _____     _____   _    _ 
-	  / ____| |  ____|     /\     |  __ \   / ____| | |  | |
-	 | (___   | |__       /  \    | |__) | | |      | |__| |
-	  \___ \  |  __|     / /\ \   |  _  /  | |      |  __  |
-	  ____) | | |____   / ____ \  | | \ \  | |____  | |  | |
-	 |_____/  |______| /_/    \_\ |_|  \_\  \_____| |_|  |_|
-	 
-	*/
-	$("#form").submit(function(){
-		
-		//2 vars from form input
-		var term = $("#search").val();
-		var attribute = $("#attr").val();
-		
-		$.getJSON("js/superheroes.json", function(result) {
+		//function that runs when form is submit
+		$("form").submit(function(){
+			var term = $("#search").val();
+			var attribute = $("#attr").val();
 			
-			//remove previous search term text if it exist before appending new search
 			$("#searchTerm").remove();
 			$("#superheroes").append("<div class='hero' id='searchTerm'>Results for: <span id='search'>"+term+"</span> in <span id='search'>"+attribute+"</span></div>");
 			//remove all superhero divs with name superhero before adding new ones from search loop
 			$("div[name='superhero']").remove();
 			
-			//use term and attribute vars from form input. Buggy at the moment, doesn't except wildcard searches.
-			$.each(result.superheroes, function(a, b) {
-				
-				if (b[attribute] == term) {
-					
+			//use term and attribute vars from form input. Doesn't except wildcard searches.
+			$.each(data.superheroes, function(k, v) {
+				if (v[attribute] == term) {
 					//sry for the messiness. Add divs that match the search term
-					$("#superheroes").append("<div class='hero' name='superhero' id="+b.superhero+">"+
-											 "<div class='details'>"+
-											 "<h1>"+b.superhero+"</h1>"+
-											 "<br>"+
-											 "<span id='publisher'>Publisher: </span>"+b.publisher+
-											 "<br>"+
-											 "<span id='alterEgo'>Alter ego: </span>"+b.alter_ego+
-											 "<br>"+
-											 "<span id='firstAppearance'>First Appearance: </span>"+b.first_appearance+
-											 "<br>"+
-											 "<span id='characters'>Portrayed by: </span>"+b.characters+
-											 "<br></div><div class='img'>"+
-											 "<img src='"+b.artURL+"'>"+
-											 "</div></div>");
+					$("#superheroes").append("<div class='hero' name='superhero' id="+v.superhero+">"+
+												 "<div class='details'>"+
+													 "<h1>"+v.superhero+"</h1>"+"<br>"+
+													 "<span id='publisher'>Publisher: </span>"+v.publisher+"<br>"+
+													 "<span id='alterEgo'>Alter ego: </span>"+v.alter_ego+"<br>"+
+													 "<span id='firstAppearance'>First Appearance: </span>"+v.first_appearance+"<br>"+
+													 "<span id='characters'>Portrayed by: </span>"+v.characters+"<br>"+
+												 "</div>"+
+											 	 "<div class='img'>"+
+												 	"<img src='"+v.artURL+"'>"+
+												 "</div>"+
+											 "</div>");
 					return;
 				} else {
 					//console.log("Nothing found");
-					//$("#superheroes").append("<div class='hero' id='searchTerm'>Didn't find anything, sry.</div>");
 				}
 			});
+			return false;
 		});
-		return false;
 	});
 });
+
